337c79c841103c51ec00d752a8fb73fc64d359d3
yamini saraswathi <103330658+yaminiakkala@users.noreply.github.com>
2023-02-12 22:46:52 +0530

    Add files via upload

---
+++ power.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+    //wap to compute the value of a^b given a and b as inputs or caluculating power
+    int a,b,c;
+    scanf("%d%d",&a,&b);
+    c=pow(a,b);
+    printf("%d",c);
+
+    return 0;
+}
+
