fd447dbb7739d0323fbd4cc3bb47bbf4f0bbf3d2
yamini saraswathi <103330658+yaminiakkala@users.noreply.github.com>
2023-02-12 22:46:35 +0530

    Add files via upload

---
+++ mulby4.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    //wap to multiply given number by 4 using bitwise operator
+    int n;
+    scanf("%d",&n);
+    printf("%d",n<<2);
+    
+
+    return 0;
+}
